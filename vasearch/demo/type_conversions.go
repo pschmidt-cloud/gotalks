86444ad98092cf9cac4f93d92893d79d0812730f
Paul Schmidt <pschmidt.mdr@gmail.com>
2016-02-03 14:03:06 -0800

    Some final additions

--- vasearch/demo/type_conversions.go
+++ vasearch/demo/type_conversions.go
@@ -6,8 +6,6 @@ import (
 	"strconv"
 )
 
-// Unlike in C, in Go assignment between items of different type requires an explicit conversion.
-
 func main() {
 	var x, y int = 3, 4
 	var f float64 = math.Sqrt(float64(x*x + y*y))
