dca3ce36779080c27d489397c9a917754b07e75e
Paul Schmidt <pschmidt.mdr@gmail.com>
2016-02-03 07:46:32 -0800

    small updates

--- vasearch/demo/slices.go
+++ vasearch/demo/slices.go
@@ -3,7 +3,7 @@ import "fmt"
 
 func main() {
 	реки := [3]string{"лена", "амур", "волга"}
-	города := []string{}
+	города := []string{"сочи"}
 
 	города = append(города, "москва") // can append to a slice but not to an array
 	fmt.Println(реки, города)
